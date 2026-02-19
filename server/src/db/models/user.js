fdcbace83eb9c771d94d984dd7ab03f35c94d076
Oleksandr <103267560+SerbynS@users.noreply.github.com>
2026-02-19 16:41:40 +0200

    feat: setup database connection and create base models (#2)
    * feat: add database connection
    
    * feat: add user model
    
    * feat: add session model
    
    * feat: add area model
    
    * feat: add categories model
    
    * feat: add ingredient model
    
    * feat: add recipe model

---
+++ server/src/db/models/user.js
@@ -0,0 +1,20 @@
+import { model, Schema } from 'mongoose';
+
+const usersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    avatar: { type: String, default: null },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    savedRecipes: [{ type: Schema.Types.ObjectId, ref: 'recipes' }],
+  },
+  { timestamps: true, versionKey: false },
+);
+
+usersSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export const UsersCollection = model('users', usersSchema);
